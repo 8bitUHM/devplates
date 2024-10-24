1abb535a10b30358f3cd4141f40a72ee49babb36
Leighton Miguel <leim56@hawaii.rr.com>
2024-10-23 17:23:57 -1000

    Update

--- webpack-typescript-react-tailwind-actions-ssh-server-deploy/src/pages/Index.tsx
+++ webpack-typescript-react-tailwind-actions-ssh-server-deploy/src/pages/Index.tsx
@@ -14,22 +14,22 @@ const Index = () => {
       <div className="container max-w-screen-xl flex justify-center flex-row flex-wrap mx-auto px-5">
         <div className="py-2">
           <h1 className="text-3xl font-extrabold text-white md:text-5xl lg:text-6xl">
-            My name is{" "}
+            This is an official{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-              Leighton Miguel
+              8bit Devplate
             </span>
           </h1>
         </div>
 
         <div className="py-2">
           <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
-            This 8bit Devplate comes with preset React, Tailwind, Webpack and GitHub
-            actions to push this static site to a server.
+            This 8bit Devplate comes with preset React, Tailwind, Webpack and
+            GitHub actions to push this static site to a server.
           </p>
         </div>
 
         <div className="py-4">
-        <img className="rounded-lg" src={logo}></img>
+          <img className="rounded-lg" src={logo}></img>
         </div>
       </div>
     </div>
